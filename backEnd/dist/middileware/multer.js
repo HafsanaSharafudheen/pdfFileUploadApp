1f20e1997955287f905c07f9130f30b2d6cfd8be
HafsanaSharafudheen <111694967+HafsanaSharafudheen@users.noreply.github.com>
2024-09-03 17:28:11 +0530

    change in multer path

--- backEnd/dist/middileware/multer.js
+++ backEnd/dist/middileware/multer.js
@@ -7,7 +7,7 @@ const multer_1 = __importDefault(require("multer"));
 const path_1 = __importDefault(require("path"));
 const storage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, path_1.default.join(__dirname, 'dist', 'uploads'));
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
