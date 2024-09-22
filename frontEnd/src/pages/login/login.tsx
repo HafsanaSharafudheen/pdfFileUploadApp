a006e04b4e7eb01e585eb14151edd2a62ac80c8f
HafsanaSharafudheen <111694967+HafsanaSharafudheen@users.noreply.github.com>
2024-09-22 09:29:53 +0530

    rechanged

--- frontEnd/src/pages/login/login.tsx
+++ frontEnd/src/pages/login/login.tsx
@@ -36,9 +36,7 @@ else{
             setError('Login failed. Please try again.');
         }
     };
-    const handleSignUp = () => {
-        navigate('/signup');
-    };
+
     return (
         <div className="main-div-container">
             <div className="loginContainer">
@@ -82,8 +80,8 @@ else{
                         <button type="submit" className="btn btn-primary">Login</button>
                     </div>
                     <p className='text-center mt-3'>
-            New user? <a href="#" onClick={handleSignUp}>Create an account</a>
-        </p>
+                        New user? <a href="/signup">Create an account</a>
+                    </p>
                 </form>
             </div>
         </div>
